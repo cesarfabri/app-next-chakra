ff2e101cbf070d23da479d38d1c2cffcf78512e3
Cesar Fabri <cesar.fabrii@gmail.com>
2022-01-30 12:18:58 -0300

    Implementation component header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import styles from "./styles.module.scss";
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Image src="/images/logo.svg" alt="adm" width={100} height={100} />
+        <nav>
+          <a className={styles.active} href="#">
+            Home
+          </a>
+          <a href="#">About</a>
+        </nav>
+      </div>
+    </header>
+  );
+}
